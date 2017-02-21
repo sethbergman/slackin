3d05d65a684dfa940dc30580820982f58a161da2
Seth Bergman <seth.atxwebs@gmail.com>
2017-02-20 23:34:08 -0600

    build

---
+++ .env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export SLACK_API_TOKEN=xoxb-140449160533-S3ruVwasyniTLGJLb6lzPTjb
+export SLACK_CLIENT_ID=3570763187.139671776449
+export SLACK_CLIENT_SECRET=39b2e09669f5a1bb0e0631b03ce1eb03
+export SLACK_VERIFY_TOKEN=9IXjoGMyWFFaqDGIvi7L2d0M
+export SLACK_SUBDOMAIN=operation-code
+export SLACK_COC=https://github.com/OperationCode/operationcode/blob/master/CODE-OF-CONDUCT.md
